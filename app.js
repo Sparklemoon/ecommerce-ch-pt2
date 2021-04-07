0da61788b4e890fbe488f53b3fb4cccb0bbd959c
Myra James <Treesmellgood@gmail.com>
2021-04-07 09:27:52 -0400

    cart content

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ class UI {
                     <img src=${product.image} alt="product" class="product-img">
                     <button class="bag-btn" data-id=${product.id}>
                         <i class="fas fa-shopping-cart"></i>
-                        add to bag
+                        add to cart
                     </button>
                 </div>
                 <h3>${product.title}</h3>
@@ -143,6 +143,15 @@ class UI {
       this.clearCart();
     });
     // cart functionality
+    cartContent.addEventListener('click',event=>{
+      if(event.target.classList.contains('remove-item'))
+      {
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        cartContent.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id);
+      }
+    });
   }
   clearCart() {
     let cartItems = cart.map(item => item.id);
@@ -152,6 +161,7 @@ class UI {
     while(cartContent.children.length>0){
       cartContent.removeChild(cartContent.children[0])
     }
+    this.hideCart();
   }
   removeItem(id) {
     cart = cart.filter(item => item.id !==id);
@@ -159,7 +169,7 @@ class UI {
     Storage.saveCart(cart);
     let button = this.getSingleButton(id);
     button.disabled = false;
-    button.innerHTML = `<i class="fas fa-shopping-cart></i>add to cart`;
+    button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`;
   }
   getSingleButton(id) {
     return buttonsDOM.find(button => button.dataset.id === id);

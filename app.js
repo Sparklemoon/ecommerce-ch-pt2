b940611ad68385775646832e35fb1cd416d3db68
Myra James <Treesmellgood@gmail.com>
2021-04-12 19:15:45 -0400

    else if statement to remove items form cart

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ class UI {
   addCartItem(){
     const div = document.createElement('div');
     div.classList.add('cart-item');
-    div.innerHTML = `<img src=${item.image} alt="product" />
+    div.innerHTML = `<img src=${item.image} alt="product"/>
     <div>
         <h4>${item.title}</h4>
         <h5>$${item.price}</h5>
@@ -158,6 +158,23 @@ class UI {
         tempItem.amount = tempItem.amount + 1;
         Storage.saveCart(cart);
         this.setCartValues(cart);
+        addAmount.nextElementSibling.innerText = tempItem.amount;
+      }
+      else if (event.target.classList.contains("fa-chevron-down")){
+        let lowerAmount = event.target;
+        let id = lowerAmount.dataset.id;
+        let tempItem = cart.find(item => item.id===id);
+        tempItem.amount = tempItem.amount - 1;
+        if (tempItem.amount > 0) {
+Storage.saveCart(cart);
+this.setCartValues(cart);
+lowerAmount.previousElementSibling.innerText = tempItem.amount;
+        }
+        else{
+          cartContent.removeChild
+          (lowerAmount.parentElement.parentElement);
+          this.removeItem(id);
+        }
       }
     });
   }
